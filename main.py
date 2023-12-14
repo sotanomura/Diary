0b2c336fa344bc012476ff6a79eb7be9b8d86ca2
KITUNE <yuyuyuya.masaki1@gmail.com>
2023-12-14 16:12:17 +0900

    正木の日記

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from diaries.DiarySample import DiarySample
+from diaries.MasakiDiary import MasakiDiary
 
 # ↓のリストには、メンバーの各日記が格納されます。
-diaries = [DiarySample(), ]
+diaries = [DiarySample(), MasakiDiary()]
 
 for d in diaries:
     print("---------------------------------")
