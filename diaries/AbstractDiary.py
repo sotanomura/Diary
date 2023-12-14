eecc501090edc7d5d5a91c7ade3ec51068121fe0
yuudati4 <104051133+yuudati4@users.noreply.github.com>
2023-12-14 14:54:45 +0900

    first commit at template

---
+++ diaries/AbstractDiary.py
@@ -0,0 +1,11 @@
+from abc import ABC, abstractmethod
+class AbstractDiary(ABC):
+    @abstractmethod
+    def get_date(self):
+        pass
+    @abstractmethod
+    def get_summary(self):
+        pass
+    @abstractmethod
+    def get_author(self):
+        pass
