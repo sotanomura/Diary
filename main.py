7cf9f84ecd8179dfc14e49a125363a086bb63b1c
Gomamono <k22057kk@aitech.ac.jp>
2023-12-14 15:50:43 +0900

    added gomamono diary

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from diaries.DiarySample import DiarySample
+from diaries.GomamonoDiary import GomamonoDiary
 
 # ↓のリストには、メンバーの各日記が格納されます。
-diaries = [DiarySample(), ]
+diaries = [DiarySample(), GomamonoDiary(), ]
 
 for d in diaries:
     print("---------------------------------")
